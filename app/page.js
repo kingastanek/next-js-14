9e3ac4b1c3cb21df688bc257edd03dbb0ca89267
Kinga Stanek <kin.stanek@gmail.com>
2024-02-02 20:07:34 +0100

    Add boilerplate to "The Foodies App"

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <h1>Meal App</h1>
+      <h1 style={{ color: "white", textAlign: "center" }}>
+        Time to get started!
+      </h1>
     </main>
   );
 }

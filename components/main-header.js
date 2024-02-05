836ba986250964608dfd32f23ea850f1d2d0d10a
Kinga Stanek <kin.stanek@gmail.com>
2024-02-05 21:50:49 +0100

    Add styles and change img to Image

--- components/main-header.js
+++ components/main-header.js
@@ -1,15 +1,19 @@
 import Link from "next/link";
+import Image from "next/image";
+
 import logoImg from "@/assets/logo.png";
+
+import classes from "./main-header.module.css";
 
 export default function MainHeader() {
   return (
-    <header>
-      <Link href="/">
-        <img src={logoImg.src} alt="A plate with food on it" />
+    <header className={classes.header}>
+      <Link href="/" className={classes.logo}>
+        <Image src={logoImg} alt="A plate with food on it" priority />
         NextLevel Food
       </Link>
 
-      <nav>
+      <nav className={classes.nav}>
         <ul>
           <li>
             <Link href="/meals">Meals</Link>

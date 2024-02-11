30a246492de73a55af08ecd36fd9ff3d020b9fd2
Kinga Stanek <kin.stanek@gmail.com>
2024-02-12 00:07:49 +0100

    Add MealsGrid and MealsItem

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,10 +1,25 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 
-export default function Meals() {
+export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals Page</h1>
-      <Link href="/meals/meal-1">Meal 1</Link>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, cerated{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }

5c86c6362833a3d78008e730409c19b4c89e6898
Kinga Stanek <kin.stanek@gmail.com>
2024-02-12 21:28:39 +0100

    Fetching meals

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -4,7 +4,7 @@ import classes from "./meals-grid.module.css";
 export default function MealsGrid({ meals }) {
   return (
     <ul className={classes.meals}>
-      {meals.map((meal) => (
+      {meals?.map((meal) => (
         <li key={meal.id}>
           <MealItem {...meal} />
         </li>

c8237d181f880ec25cfcd0e74ca1312fe1e3fbff
Kinga Stanek <kin.stanek@gmail.com>
2024-02-15 23:06:30 +0100

    Add error handling

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>Oops, something went wrong!</h1>
+      <p>Failed to fetch meal data. Please try again later!</p>
+    </main>
+  );
+}

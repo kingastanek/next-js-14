9d3c1f3549365c8ccc795ca54a47b36fa3b0f767
Kinga Stanek <kin.stanek@gmail.com>
2024-02-27 23:06:12 +0100

    Add server action to share meal

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -2,6 +2,27 @@ import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 
 export default function ShareMealPage() {
+  async function shareMeal(formData) {
+    "use server";
+    const title = formData.get("title");
+    const summary = formData.get("summary");
+    const instructions = formData.get("instructions");
+    const image = formData.get("image");
+    const creator = formData.get("name");
+    const creator_email = formData.get("email");
+
+    const meal = {
+      title,
+      summary,
+      instructions,
+      image,
+      creator,
+      creator_email,
+    };
+
+    console.log("meal", meal);
+  }
+
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +32,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
